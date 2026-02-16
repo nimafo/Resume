6799d1c4669d5a30877fdc4540e038ab18261750
nimafo <45439717+nimafo@users.noreply.github.com>
2026-02-16 14:47:48 +0000

    Deploying to gh-pages from @ nimafo/Resume@3f368e7458409f3998ce706c1313f611b506878c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,13 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/Resume/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Course materials, schedules, and resources for classes taught.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/Resume/teaching/";
-          },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
               description: "",
