80e14b69c72d5cc7fab2c3724307fe36fc441277
nimafo <45439717+nimafo@users.noreply.github.com>
2026-02-16 12:11:54 +0000

    Deploying to gh-pages from @ nimafo/Resume@cc720bfd58efcb84c6b9ffd268f608b8ef8f2ef2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -449,16 +449,6 @@ ninja.data = [{
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/Resume/projects/1_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/Resume/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/Resume/projects/6_project/";
             },},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
